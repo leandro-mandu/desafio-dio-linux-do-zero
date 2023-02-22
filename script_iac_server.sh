deef24c25801375f887040bdec50cd7b90235ec5
leandro-mandu <leandro.om15@gmail.com>
2023-02-22 18:45:14 -0300

    iac de provisionamento de servidor apache2

---
+++ script_iac_server.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#atualizar servidor
+apt-get update
+apt-get upgrade -y
+#baixar apache2
+apt-get install apache2 -y
+#baixar unzip
+apt-get install unzip -y
+#baixar site zip na /tmp
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+#provisionar site no server
+cp -R linux-site-dio/* /var/www/html/
+
